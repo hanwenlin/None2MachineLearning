6883ed970fc40ac5fca034975fa48898d9bede37
hanwenlin <1353516732@qq.com>
2019-01-03 14:58:20 +0800

    Add files via upload

---
+++ Chapter1_Introduction/comments.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+# a function which sum of two numbers
+def adds(a,b):
+    """
+    sums two numbers.
+    >>> add(1,2)
+    3
+    """
+    return a+b
+
+print(adds(4,6))  # print two numbers
+print(adds.__doc__)
